939976527daec9a83285cf41fa2351b724c75fe8
Serim Min <97885933+anonymousRecords@users.noreply.github.com>
2025-01-07 15:39:59 +0900

    refactor: change font file direction (#18)

--- apps/chat/app/layout.tsx
+++ apps/chat/app/layout.tsx
@@ -1,33 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import localFont from "next/font/local";
+import { pretendard } from '@/public/fonts/font';
 import 'swagger-ui-react/swagger-ui.css';
-
-const pretendard = localFont({
-  src: [
-    {
-      path: '../public/fonts/Pretendard-Regular.woff2',
-      weight: '400',
-      style: 'normal',
-    },
-    {
-      path: '../public/fonts/Pretendard-Medium.woff2',
-      weight: '500',
-      style: 'normal',
-    },
-    {
-      path: '../public/fonts/Pretendard-SemiBold.woff2',
-      weight: '700',
-      style: 'normal',
-    },
-    {
-      path: '../public/fonts/Pretendard-ExtraBold.woff2',
-      weight: '900',
-      style: 'normal',
-    },
-  ],
-  variable: '--font-pretendard',
-});
 
 export const metadata: Metadata = {
   title: "Swagger Chat",
