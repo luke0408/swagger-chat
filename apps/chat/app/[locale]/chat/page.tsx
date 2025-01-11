89b72d1146c7390723f4cd51421db3e536d68729
anonymousrecords <a.Records2022@gmail.com>
2025-01-11 20:19:45 +0900

    feat: chat page

---
+++ apps/chat/app/[locale]/chat/page.tsx
@@ -0,0 +1,9 @@
+import WindowContainer from '@/components/layouts/window-container';
+
+export default function Home() {
+  return (
+    <main className="min-h-screen">
+      <WindowContainer />
+    </main>
+  );
+}
